a325525bfc5c2dd4df45e4a5315ac3986c43dd0e
Yaswant <yaswantsoni2005@gmail.com>
2025-09-14 23:32:14 +0530

    setup: react native with native wind

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+import "../global.css"
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="text-black bg-black font-bold text-red-500">Edit app/index.tsx to edit this screen cgcf fg  gfgfg fbdfd. xfdfd</Text>
+    </View>
+  );
+}
